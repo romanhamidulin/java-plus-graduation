ee7822c32a6094824b9ec9b8c00536418ceeb527
Роман Хамидулин <romanhamidulin@ya.ru>
2026-03-23 12:35:03 +0500

    Фикс ошибки

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStats;
@@ -17,11 +18,11 @@ public interface StatsClient {
 
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
-    void createHit(@Valid @RequestBody EndpointHitDto hitDto) throws FeignException;
+    void hit(@Valid @RequestBody EndpointHitDto hitDto) throws FeignException;
 
     @GetMapping("/stats")
-    List<ViewStats> getStat(@RequestParam(name = "start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-                               @RequestParam(name = "end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
-                               @RequestParam(name = "uris", required = false) List<String> uris,
-                               @RequestParam(name = "unique", defaultValue = "false") Boolean unique) throws FeignException;
+    ResponseEntity<List<ViewStats>> getStats(@RequestParam(name = "start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                             @RequestParam(name = "end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                             @RequestParam(name = "uris", required = false) List<String> uris,
+                                             @RequestParam(name = "unique", defaultValue = "false") Boolean unique) throws FeignException;
 }
