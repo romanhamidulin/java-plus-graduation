3f324d2068dc28abd20967f00d0c4777ab4e298d
Роман Хамидулин <romanhamidulin@ya.ru>
2026-03-30 11:31:37 +0500

    Фикс ошибки сборки

--- core/comment-service/src/main/java/ru/practicum/mapper/CommentMapper.java
+++ core/comment-service/src/main/java/ru/practicum/mapper/CommentMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.*;
 import ru.practicum.dto.comment.CommentDto;
 import ru.practicum.dto.comment.NewCommentDto;
 import ru.practicum.model.Comment;
+import ru.practicum.enums.CommentStatus;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
